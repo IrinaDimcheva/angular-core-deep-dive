dd259feba122c54feadc6351d44ec27dfd9dffe8
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-17 11:25:58 +0200

    Built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,12 @@ export class AppComponent {
   courses = COURSES;
   // courses = [];
 
+  title = COURSES[0].description;
+  price = 9.99;
+  rate = 0.67;
+  startDate = new Date(2000, 0, 1);
+  course = COURSES[0];
+
   selectCourse(course: Course) {
     console.log('App component - click event bubbled', course);
   }
