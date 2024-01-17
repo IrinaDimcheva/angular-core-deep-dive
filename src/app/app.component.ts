395d23a1b71ff0bf25a9d005b904eb2b59a53605
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-17 14:25:56 +0200

    afterViewInit lifecycle hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,31 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
-import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   courses = COURSES;
   // courses = [];
 
-  // @ViewChild(CourseCardComponent) card: CourseCardComponent;
-  @ViewChild('cardRef1', { read: ElementRef }) card: ElementRef;
-  @ViewChild('cardRef1') card1: CourseCardComponent;
-  @ViewChild('cardRef2') card2: CourseCardComponent;
-  @ViewChild('container') containerDiv: ElementRef;
+  @ViewChild('cardRef1', { read: ElementRef }) card1: ElementRef;
+  @ViewChild('courseImage') courseImage: ElementRef;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    // console.log('courseImage', this.courseImage); - undefined | not part of this component
+    // console.log('ContainerDiv', this.card1);
+    // Throws an error
+    // this.courses[0].description = 'test';
+  }
 
   selectCourse(course: Course) {
-    // console.log('Card1', this.card1);
-    // console.log('Card2', this.card2);
-    console.log('ContainerDiv', this.containerDiv);
-    console.log(this.card);
+    // console.log('ContainerDiv', this.containerDiv);
+    // console.log('ContainerDiv', this.card1);
   }
 
   trackCourse(index: number, course: Course) {
