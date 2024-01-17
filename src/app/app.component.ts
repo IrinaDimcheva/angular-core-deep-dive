bbadf3e6a37075f097c3b431872f3b70d7261e59
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-17 16:56:14 +0200

    Content child decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,6 @@ export class AppComponent implements AfterViewInit {
   courses = COURSES;
   // courses = [];
 
-  // @ViewChildren(CourseCardComponent) cards: QueryList<CourseCardComponent>;
   @ViewChildren(CourseCardComponent, { read: ElementRef })
   cards: QueryList<ElementRef>;
 
@@ -26,21 +25,6 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     console.log(this.cards);
-    // console.log(this.cards.first);
-    // this.cards.changes.subscribe((cards) => console.log(cards));
-  }
-
-  editCourses() {
-    this.courses.push({
-      id: 1,
-      description: 'Angular Core Deep Dive',
-      iconUrl:
-        'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
-      longDescription:
-        'A detailed walk-through of the most important part of Angular - the Core and Common modules',
-      category: 'INTERMEDIATE',
-      lessonsCount: 10,
-    });
   }
 
   selectCourse(course: Course) {}
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,6 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Course } from '../model/course';
+import { CourseImageComponent } from '../course-image/course-image.component';
 
 @Component({
   selector: 'course-card',
@@ -9,19 +18,26 @@ import { Course } from '../model/course';
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css',
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements AfterViewInit {
   @Input({ required: true }) course: Course;
   // @Input({ required: true }) index: number;
   @Input() cardIndex: number;
   @Output() courseSelected = new EventEmitter<Course>();
   // @Output('courseSelected') courseEmitter = new EventEmitter<Course>();
 
+  // @ContentChild(CourseImageComponent) image: CourseImageComponent;
+  @ContentChild(CourseImageComponent, { read: ElementRef })
+  image: ElementRef;
+
+  ngAfterViewInit(): void {
+    console.log(this.image);
+  }
+
   isImageVisible() {
     return this.course && this.course.iconUrl;
   }
 
   viewCourse() {
-    console.log('card component - button clicked...');
     this.courseSelected.emit(this.course);
     // this.courseEmitter.emit(this.course);
   }
